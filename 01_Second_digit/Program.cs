4c2c50722b39a3985a873c77fbd9f65f6d2ba824
Anna <ann.sxd@example.com>
2022-10-24 21:37:27 +0300

    1 задача

---
+++ 01_Second_digit/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход 
+// трёхзначное число и на выходе показывает вторую цифру этого числа
+
+int GetSecondDigit(int number)
+{
+    return (number /10) % 10;
+}
+
+Console.Write("Введите трёхзначное число: ");
+int Number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(GetSecondDigit(Number));
